11049713b50c02d6d82262d5448029f25f3e8931
Matheus <matheus.pormanhani@gmail.com>
2022-04-19 21:42:25 -0300

    chore: small cleanup

--- app/backend/src/tests/clubs.test.ts
+++ app/backend/src/tests/clubs.test.ts
@@ -4,7 +4,6 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 
 import { Response } from 'superagent';
-import { verifyToken } from '../utils/tokenHelper';
 import { IClub } from '../utils/interfaces';
 
 chai.use(chaiHttp);
