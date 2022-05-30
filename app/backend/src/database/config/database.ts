84d1efb3373b1ecf5a87eaca26df5ed93aea02a9
Matheus <matheus.pormanhani@gmail.com>
2022-05-30 15:17:25 -0300

    fix: add blank .env

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -4,8 +4,8 @@ module.exports = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '123456',
   database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || '3002',
+  host: process.env.DB_HOST || 'db',
+  port: process.env.DB_PORT || '3306',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
